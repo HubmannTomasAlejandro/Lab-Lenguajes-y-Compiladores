f40d2a59e62d58187ae673d47b0e89dda042767c
Tomas Hubmann <thubmann@seenka.com>
2025-06-02 20:21:07 -0300

    Cambiar orden de codigo y readme

--- Lab3.hs
+++ Lab3.hs
@@ -1,11 +1,6 @@
 {-# LANGUAGE GADTs, TypeSynonymInstances, FlexibleInstances #-}
 import Control.Applicative ( liftA, liftA2 )
 
---         ∞
--- fix f = ⊔ fⁱ ⊥
---        i=0
-fix :: (a -> a) -> a
-fix f = f (fix f)
 
 type Var = String
 type Σ   = Var -> Int
@@ -23,75 +18,49 @@ data Ω = Normal Σ | Abort Σ | Out (Int, Ω) | In (Int -> Ω)
    * In     : (Z → Ω) → Ω
 -}
 
-update :: Σ -> Var -> Int -> Σ
-update σ v n v' = if v == v' then n else σ v'
-
 data Expr a where
   -- # Expresiones enteras
-  Const :: Int       -> Expr MInt                -- n
-  Var    :: Var        -> Expr MInt                -- v
-  Plus :: Expr MInt  -> Expr MInt -> Expr MInt   -- e + e'
-  Prod :: Expr MInt  -> Expr MInt  -> Expr MInt
-  Div  :: Expr MInt  -> Expr MInt  -> Expr MInt
-  Op   :: Expr MInt  -> Expr MInt
+  Const :: Int         -> Expr MInt                          -- n
+  Var   :: Var         -> Expr MInt                          -- v
+  Plus  :: Expr MInt   -> Expr MInt  -> Expr MInt            -- e + e'
+  Prod  :: Expr MInt   -> Expr MInt  -> Expr MInt            -- e * e'
+  Div   :: Expr MInt   -> Expr MInt  -> Expr MInt            -- e / e'
+  Op    :: Expr MInt   -> Expr MInt                          -- - e
   -- # Expresiones booleanas
-  CBool:: Bool -> Expr MBool
-  Not  :: Expr MBool -> Expr MBool
-  And  :: Expr MBool -> Expr MBool -> Expr MBool
-  Or   :: Expr MBool -> Expr MBool -> Expr MBool
+  CBool :: Bool        -> Expr MBool                          -- b
+  Not   :: Expr MBool  -> Expr MBool                          -- ¬ b
+  And   :: Expr MBool  -> Expr MBool -> Expr MBool            -- b ∧ b'
+  Or    :: Expr MBool  -> Expr MBool -> Expr MBool            -- b ∨ b'
   -- Comparacion
-  Eq   :: Expr MInt  -> Expr MInt -> Expr MBool -- e = e'
-  Lt   :: Expr MInt  -> Expr MInt -> Expr MBool -- e < e'
-  Gt   :: Expr MInt  -> Expr MInt  -> Expr MBool
-  GtE  :: Expr MInt  -> Expr MInt  -> Expr MBool
-  LtE  :: Expr MInt  -> Expr MInt  -> Expr MBool
-
+  Eq    :: Expr MInt   -> Expr MInt  -> Expr MBool            -- e = e'
+  Lt    :: Expr MInt   -> Expr MInt  -> Expr MBool            -- e < e'
+  Gt    :: Expr MInt   -> Expr MInt  -> Expr MBool            -- e > e'
+  GtE   :: Expr MInt   -> Expr MInt  -> Expr MBool            -- e >= e'
+  LtE   :: Expr MInt   -> Expr MInt  -> Expr MBool            -- e <= e'
   -- # Comandos LIS
-  Skip   :: Expr Ω                                        -- skip
-  Assign :: Var        -> Expr MInt -> Expr Ω                -- v := e
-  Seq    :: Expr Ω     -> Expr Ω -> Expr Ω                -- c ; c'
-  Cond   :: Expr MBool -> Expr Ω -> Expr Ω -> Expr Ω      -- if b then c else c'
-  Newvar :: Var        -> Expr MInt -> Expr Ω -> Expr Ω      -- newvar v := e in e'
-  While  :: Expr MBool -> Expr Ω -> Expr Ω                -- while b do c
-
+  Skip   :: Expr Ω                                            -- skip
+  Assign :: Var        -> Expr MInt  -> Expr Ω                -- v := e
+  Seq    :: Expr Ω     -> Expr Ω     -> Expr Ω                -- c ; c'
+  Cond   :: Expr MBool -> Expr Ω     -> Expr Ω -> Expr Ω      -- if b then c else c'
+  Newvar :: Var        -> Expr MInt  -> Expr Ω -> Expr Ω      -- newvar v := e in e'
+  While  :: Expr MBool -> Expr Ω     -> Expr Ω                -- while b do c
   -- # Comandos Fallas
-  Fail   :: Expr Ω                                        -- fail
-  Catch  :: Expr Ω -> Expr Ω -> Expr Ω                    -- catch c with c'
-
+  Fail   :: Expr Ω                                            -- fail
+  Catch  :: Expr Ω     -> Expr Ω     -> Expr Ω                -- catch c with c'
   -- # Comandos IO
-  SOut :: Expr MInt -> Expr Ω                              -- !e
-  SIn  :: Var -> Expr Ω                              -- ?v
+  SOut   :: Expr MInt  -> Expr Ω                              -- !e
+  SIn    :: Var        -> Expr Ω                              -- ?v
 
-class DomSem dom where
-  sem :: Expr dom -> Σ -> dom
+{- Funciones auxiliares para la semantica -}
+--         ∞
+-- fix f = ⊔ fⁱ ⊥
+--        i=0
+fix :: (a -> a) -> a
+fix f = f (fix f)
 
-instance DomSem MInt where
-  -- Completar
-  sem (Const a)    = \_ -> Just a
-  sem (Var v)      = \σ -> Just $ σ v
-  sem (Plus e1 e2) = \σ -> ((+)-^-) (sem e1 σ) (sem e2 σ)
-  sem (Op e)       = \σ ->  (negate -^) (sem e σ)
-  sem (Prod e1 e2) = \σ ->  ((*) -^-) (sem e1 σ)  (sem e2 σ)
-  sem (Div e1 e2)  = \σ ->  if (sem e2 σ)  == Just 0
-                              then Nothing
-                              else ((div) -^-) (sem e1 σ)  (sem e2 σ)
-
-instance DomSem MBool where
-  -- Completar
-  sem (Eq e1 e2) = \σ -> ((==)-^-) (sem e1 σ) (sem e2 σ)
-  sem (Lt e1 e2) = \σ -> ((<)-^-) (sem e1 σ) (sem e2 σ)
-  sem (CBool b)  = \σ -> Just b
-  sem (Not e)    = \σ -> (not -^) (sem e σ)
-  sem (And e1 e2)= \σ -> ((&&) -^-) (sem e1 σ) (sem e2 σ)
-  sem (Or e1 e2 )= \σ -> ((||) -^-) (sem e1 σ)  (sem e2 σ)
-  sem (Gt e1 e2 )= \σ -> ((>) -^-) (sem e1 σ)  (sem e2 σ)
-  sem (GtE e1 e2)= \σ -> ((>=) -^-) (sem e1 σ)  (sem e2 σ)
-  sem (LtE e1 e2)= \σ -> ((<=) -^-) (sem e1 σ)  (sem e2 σ)
-
-{-
 update :: Σ -> Var -> Int -> Σ
 update σ v n v' = if v == v' then n else σ v'
--}
+
 rest :: Σ -> Var -> (Σ -> Σ)
 rest σ v σ' v'= if v == v' then σ v else σ' v'
 
@@ -101,23 +70,11 @@ f_big b c = \f -> \σ -> (case (sem b σ) of
     Just True  -> (f *.) (sem c σ)
     _ -> Abort σ)
 
-instance DomSem Ω where
-  -- Completar
-  sem Skip           = \σ -> Normal σ
-  sem (Assign v e)   = \σ -> (sem e σ, σ) >>== (\val -> Normal(update σ v val))                       -- v := e
-  sem (Seq c1 c2)    = \σ -> ((sem c2) *.) (sem c1 σ)                                                 -- c ; c'
-  sem (Cond b c1 c2) = \σ -> (sem b σ, σ) >>== (\bv -> if bv then sem c1 σ else sem c2 σ)             -- if b then c else c'
-  sem (Newvar v e c) = \σ -> (sem e σ, σ) >>== (\val -> ((rest σ v) †.) (sem c (update σ v val)))     -- newvar v := e in e'
-  sem (While b c)   = fix (f_big b c)                                                     -- while b do c
-  -- # Comandos FallasNormal
-  sem Fail           = \σ -> Abort σ       -- fail
-  sem (Catch c1 c2)  = \σ -> ((sem c2) +.) (sem c2 σ)       -- catch c with c'
-  -- # Comandos IONormal
-  sem (SOut e)       = \σ -> (sem e σ, σ) >>== (\val -> Out(val , Normal σ))         -- !e
-  sem (SIn  v)       = \σ -> In(\n -> Normal(update σ v n))       -- ?v
+(-^-) :: (a -> b -> c) -> (Maybe a -> Maybe b -> Maybe c)
+(-^-) = liftA2
 
-
---    * In     : (Z → Ω) → Ω
+(-^) :: (a -> b) -> (Maybe a -> Maybe b)
+(-^) = liftA
 
 (>>==) :: (Maybe a, Σ) -> (a -> Ω) -> Ω
 (>>==) (Nothing, σ) _ = Abort σ
@@ -141,6 +98,48 @@ instance DomSem Ω where
 (+.) f (Out (n,ω)) = Out (n, f +. ω)
 (+.) f (In g)      = In ((f +.) . g)
 
+
+{- Semántica de evaluación -}
+class DomSem dom where
+  sem :: Expr dom -> Σ -> dom
+
+instance DomSem MInt where
+  sem (Const a)    = \_ -> Just a
+  sem (Var v)      = \σ -> Just $ σ v
+  sem (Plus e1 e2) = \σ -> ((+)-^-) (sem e1 σ) (sem e2 σ)
+  sem (Op e)       = \σ ->  (negate -^) (sem e σ)
+  sem (Prod e1 e2) = \σ ->  ((*) -^-) (sem e1 σ)  (sem e2 σ)
+  sem (Div e1 e2)  = \σ ->  if (sem e2 σ)  == Just 0
+                              then Nothing
+                              else ((div) -^-) (sem e1 σ)  (sem e2 σ)
+
+instance DomSem MBool where
+  sem (Eq e1 e2) = \σ -> ((==)-^-) (sem e1 σ) (sem e2 σ)
+  sem (Lt e1 e2) = \σ -> ((<)-^-) (sem e1 σ) (sem e2 σ)
+  sem (CBool b)  = \σ -> Just b
+  sem (Not e)    = \σ -> (not -^) (sem e σ)
+  sem (And e1 e2)= \σ -> ((&&) -^-) (sem e1 σ) (sem e2 σ)
+  sem (Or e1 e2 )= \σ -> ((||) -^-) (sem e1 σ)  (sem e2 σ)
+  sem (Gt e1 e2 )= \σ -> ((>) -^-) (sem e1 σ)  (sem e2 σ)
+  sem (GtE e1 e2)= \σ -> ((>=) -^-) (sem e1 σ)  (sem e2 σ)
+  sem (LtE e1 e2)= \σ -> ((<=) -^-) (sem e1 σ)  (sem e2 σ)
+
+instance DomSem Ω where
+  sem Skip           = \σ -> Normal σ
+  sem (Assign v e)   = \σ -> (sem e σ, σ) >>== (\val -> Normal(update σ v val))                       -- v := e
+  sem (Seq c1 c2)    = \σ -> ((sem c2) *.) (sem c1 σ)                                                 -- c ; c'
+  sem (Cond b c1 c2) = \σ -> (sem b σ, σ) >>== (\bv -> if bv then sem c1 σ else sem c2 σ)             -- if b then c else c'
+  sem (Newvar v e c) = \σ -> (sem e σ, σ) >>== (\val -> ((rest σ v) †.) (sem c (update σ v val)))     -- newvar v := e in e'
+  sem (While b c)    = fix (f_big b c)                                                                -- while b do c
+  -- # Comandos Fallas
+  sem Fail           = \σ -> Abort σ                                                                  -- fail
+  sem (Catch c1 c2)  = \σ -> ((sem c2) +.) (sem c2 σ)                                                 -- catch c with c'
+  -- # Comandos IO
+  sem (SOut e)       = \σ -> (sem e σ, σ) >>== (\val -> Out(val , Normal σ))                          -- !e
+  sem (SIn  v)       = \σ -> In(\n -> Normal(update σ v n))                                           -- ?v
+
+
+
 {- Funciones de evaluación de dom -}
 
 class Eval dom where
@@ -160,24 +159,16 @@ instance Eval Ω where
           unrollOmega (Out (n, ω)) = putStrLn (show n) >> unrollOmega ω
           unrollOmega (In f)       = getLine >>= unrollOmega . f . read
 
-{- Funciones auxiliares -}
-(-^-) :: (a -> b -> c) -> (Maybe a -> Maybe b -> Maybe c)
-(-^-) = liftA2
-
-(-^) :: (a -> b) -> (Maybe a -> Maybe b)
-(-^) = liftA
-
+{-Ejemplo de uso de la semántica de evaluación-}
 
 main = do
-  let f x = 2
-  print $ sem (Prod (Const 5) (Const 3)) f
-  print $ sem (CBool True) f
-  print $ sem (Not (CBool False)) f
-  print $ sem (And (CBool True) (CBool False)) f
-  print $ sem (Eq (Const 3) (Const 4)) f
-  print $ sem (Eq (Div (Const 10) (Const 0)) (Const 5)) f
-  print $ sem (Eq (Div (Const 10) (Var "x")) (Const 5)) f
-
+  eval (Prod (Const 5) (Const 3)) (\_ -> 0)
+  eval (CBool True) (\_ -> 0)
+  eval (Not (CBool False)) (\_ -> 0)
+  eval (And (CBool True) (CBool False)) (\_ -> 0)
+  eval (Eq (Const 3) (Const 4)) (\_ -> 0)
+  eval (Eq (Div (Const 10) (Var "x")) (Const 5)) (\_ -> 0)
+  eval (Eq (Div (Const 10) (Var "x")) (Const 5)) (\_ -> 2)
 
 ej1 :: Expr Ω
 ej1 = While (Lt (Var "x") (Const 10)) $
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Lab-Lenguajes-y-Compiladores
-Implementacion del lenguaje de la materia con Haskell
+# Lab - Lenguajes y Compiladores
+
+A lo largo de este archivo se realiza la implementación del lenguaje de la materia con Haskell, salvo los operadores **para todo** y **existe** para booleanos. El código se divide en varias partes:
+
+## 1. Definición de tipos y sintaxis
+
+En primer lugar, se definen los tipos para `Var`, `Σ` y los dominios semánticos. Luego se especifica la manera en la que se construye la sintaxis de las expresiones de los distintos dominios.
+
+## 2. Funciones auxiliares
+
+A continuación, se presentan las definiciones de funciones auxiliares que se utilizarán para dar la semántica de los distintos dominios. Entre estas se encuentran algunas funciones vistas en la parte teórica, como:
+
+- `resty` y `f_big`, que se usan para que la semántica sea dirigida por la sintaxis.
+- `(*.)`, `(†.)` y `(+.)`, que se definen para garantizar la propagación del comportamiento ante errores.
+
+También se incluyen funciones para manejar los problemas de tipos que se presentan al utilizar `MInt` y `MBool` para representar los dominios semánticos de los enteros y booleanos, respectivamente:
+
+- `>>==`
+- `-^-`
+- `-^`
+
+Finalmente, se define la función `fix`, que se utiliza para obtener el menor punto fijo de una función, y la función `eval`, usada para modificar el estado que recibe la semántica a lo largo de la evaluación.
+
+## 3. Semánticas de los dominios
+
+Luego se encuentran las definiciones de las semánticas de los distintos dominios. Estas incluyen:
+
+- La semántica de expresiones de tipo `MInt` y `MBool`.
+- La semántica de expresiones de tipo `Ω`.
+
+## 4. Evaluación y pruebas
+
+Finalmente, se encuentra la definición de `eval` para los distintos dominios semánticos. Esta es una función que recibe una expresión del dominio y un estado, y realiza **acciones observables** (como imprimir valores), sin devolver un resultado útil.
+
+Además, se incluye una **lista de tests** que se utilizan para verificar el correcto funcionamiento de las semánticas.
